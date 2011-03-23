24a2605fc64350e40eacdda9b1268315587c9192
Lulu API <api.code@lulu.com>
2011-03-23 14:01:28 -0400

    add concept of file id to the file details pojo

--- src/main/java/com/lulu/publish/model/FileDetails.java
+++ src/main/java/com/lulu/publish/model/FileDetails.java
@@ -1,10 +1,23 @@
 package com.lulu.publish.model;
 
+import org.codehaus.jackson.annotate.JsonProperty;
+
 public class FileDetails {
     public static final String MIME_TYPE_PDF = "application/x-pdf";
 
+    private long fileId;
     private String mimetype;
     private String filename;
+
+    @JsonProperty("file_id")
+    public long getFileId() {
+        return fileId;
+    }
+
+    @JsonProperty("file_id")
+    public void setFileId(long fileId) {
+        this.fileId = fileId;
+    }
 
     public String getMimetype() {
         return mimetype;
@@ -25,8 +38,9 @@ public class FileDetails {
     @Override
     public String toString() {
         return "FileDetails{"
-                + "filename='" + filename + '\''
+                + "fileId=" + fileId
                 + ", mimetype='" + mimetype + '\''
+                + ", filename='" + filename + '\''
                 + '}';
     }
 }
